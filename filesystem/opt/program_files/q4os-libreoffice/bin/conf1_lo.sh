592a0b7cc81a83e56ed5fcf90da419666e286379
Q4OS Team <q4os@q4os.org>
2026-04-21 11:48:35 +0200

    Initial commit

---
+++ filesystem/opt/program_files/q4os-libreoffice/bin/conf1_lo.sh
@@ -0,0 +1,30 @@
+#this code is sourced from /usr/lib/libreoffice/program/ooenv script anytime libreoffice starts
+LOGFL1="/tmp/.conf1losh.log"
+echo "[DBG:] Starting conf1_lo.sh snippet ..." > $LOGFL1
+if [ "$QDSK_SESSION" = "trinity" ] ; then
+  F_USR_XDG_CFG_TDE="$( dash /usr/share/apps/q4os_system/bin/print_xdgcfghome_tde.sh )"
+  if [ -z "$F_USR_XDG_CFG_TDE" ] && [ -d "$HOME/.configtde/" ] ; then
+    F_USR_XDG_CFG_TDE="$HOME/.configtde"
+  fi
+  if [ -z "$F_USR_XDG_CFG_TDE" ] ; then
+    F_USR_XDG_CFG_TDE="/tmp/.xdgcfg_fake"
+  fi
+  echo "[DBG:] XDG config home: >$F_USR_XDG_CFG_TDE<" >> $LOGFL1
+  CFG_FILE1="$F_USR_XDG_CFG_TDE/libreoffice/4/user/registrymodifications.xcu"
+  if [ ! -f "$CFG_FILE1" ] ; then
+    echo "[DBG:] Libreoffice configfile: >$CFG_FILE1<" >> $LOGFL1
+    mkdir -p "$( dirname $CFG_FILE1 )"
+    if [ "$( dash /usr/share/apps/q4os_system/bin/print_package_version.sh "libreoffice-style-colibre" )" != "0" ] ; then
+      echo "[DBG:] Writing modifications registrymodifications.xcu.colibre ..." >> $LOGFL1
+      cp /opt/program_files/q4os-libreoffice/share/registrymodifications.xcu.colibre $CFG_FILE1
+    elif [ "$( dash /usr/share/apps/q4os_system/bin/print_package_version.sh "libreoffice-style-breeze" )" != "0" ] ; then
+      echo "[DBG:] Writing modifications registrymodifications.xcu.breeze ..." >> $LOGFL1
+      cp /opt/program_files/q4os-libreoffice/share/registrymodifications.xcu.breeze $CFG_FILE1
+    elif [ "$( dash /usr/share/apps/q4os_system/bin/print_package_version.sh "libreoffice-style-galaxy" )" != "0" ] ; then
+      echo "[DBG:] Writing modifications registrymodifications.xcu.galaxy ..." >> $LOGFL1
+      cp /opt/program_files/q4os-libreoffice/share/registrymodifications.xcu.galaxy $CFG_FILE1
+    else
+      echo "[DBG:] Modifications has not been written." >> $LOGFL1
+    fi
+  fi
+fi
---
+++ filesystem/opt/trinity/env/50_oooenv.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#this script is sourced from starttde
+export OOO_FORCE_DESKTOP="gtk3" #libreoffice ui looks ugly for kde5 engine in tde
+# if [ "$QAPTDISTR" = "bookworm" ] ; then
+#   export OOO_FORCE_DESKTOP="kde5" #todo: set gtk3, for classic looks, where fonts are not antialiased
+# fi
